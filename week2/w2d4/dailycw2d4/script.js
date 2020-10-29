2e1a5ba455d0093058fbcb6a4421cc4da75e0193
lea soussan <leasoussan@hmail.com>
2020-10-29 23:50:08 +0200

    correction made

--- week2/w2d4/dailycw2d4/script.js
+++ week2/w2d4/dailycw2d4/script.js
@@ -17,17 +17,67 @@
 
 // To do this challenge you only need Javascript(No HTML and no CSS)
 
-let entry = prompt("What do you like most to do   seperate every word with coma?");
-   
-function starWords (sentence){
-    let words = entry.split(',').join('\r\n');
-    let longw = 0; 
 
-   for (let w = 0; w < words.length; w++){
-       if (entry[w].length > longw){
-           longw = entry[w].length 
-       }
-       return longw
+function starWords (){
+    let getsentence = prompt("What do you like most -seperate by coma");
+    let splitw = getsentence.split(',')
+    
+    let border = "*".repeat(getsentence.length)
+    console.log(border)
+    for (word of splitw){
+        console.log("*" + " " + word + " ".repeat(getsentence.length - word.length -3) + "*") 
+        // the -3 are the stars that are att eh stars to allign to the left 
+
     }
+    console.log(border)
 }
-console.log(starWords(entry))
+
+
+// ------OTHER SOLUTION-----------------------
+// function framedString (phrase) {
+//     let arr = phrase.split(", ")
+//     let longest = 0;
+// ​
+//     for (w of arr) {
+//         if (w.length > longest) {
+//             longest = w.length
+//         }
+//     }
+//     let line = ""
+//     let something = ""
+//     for (i=0; i<longest+4; i++) {
+//         line += "*"
+//     }
+//     console.log(line)
+// ​
+//     for (word of arr) {
+//         let numMarginR = longest +1 -word.length;
+//         rightSpaces = ""
+//         for (i=0; i<numMarginR; i++) {
+//             rightSpaces += " ";
+//         }
+//         console.log("* " + word + rightSpaces + "*")
+//     }
+//     console.log(line)
+// }
+// ​
+// framedString("Hello, how, are, beauiful, you")
+
+
+
+
+
+starWords()
+   
+// function starWords (sentence){
+//     let words = sentence.split(',').join('\r\n');
+//     let longw = 0; 
+
+//    for (let w = 0; w < words.length; w++){
+//        if (entry[w].length > longw){
+//            longw = entry[w].length 
+//        }
+//        return longw
+//     }
+
+// /console.log(starWords(prompt("What do you like most to do   seperate every word with coma?")))
