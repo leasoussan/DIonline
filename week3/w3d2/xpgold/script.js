2186dee1649a712d5f2580e69d71df73d71fa4fc
lea soussan <leasoussan@hmail.com>
2020-11-02 23:14:59 +0200

    xp gold advanced

--- week3/w3d2/xpgold/script.js
+++ week3/w3d2/xpgold/script.js
@@ -51,29 +51,65 @@
 // Click on the “Number of items” button to show the number of items in your list.
 // The one and only element on the HTML file is :
 
+//reaching to the root- creat input place holder
 let shooping = document.getElementById("root");
 let list = document.createElement("input");
 list.setAttribute("type", "text");
-list.setAttribute("placeholder", "enter items");
+list.setAttribute("onfocus", "this.value=''");
+// this is to get a clear input after adding 
+
 
 shooping.append(list)
+// console.log(list)
 
-console.log(list)
-
-
+//adding button and accessing it
 let add = document.createElement("button")
 add.innerHTML = "add"
+add.setAttribute("id", "button")
 shooping.appendChild(add)
 
-add.addEventListener("click", function(){
-    add= document.querySelector("text");
-    add.appendChild(list);
-})
+console.log(add)
 
 
-// function add() {
-//     data.push({ name: userinput.value });
-//     userinput.value='';
-//     refresh();
-//   }
+let addTolist = document.getElementById("button");
 
+function addingOn() {
+        
+        for (i = 0; i< shooping.length ; i++);
+        let item = document.createElement("li");
+        item.innerHTML= document.querySelector("input").value;
+        // item.innerHTML = list.value; works as well
+        shooping.appendChild(item);
+};
+
+
+addTolist.addEventListener('click', addingOn);
+
+
+//adding resset Button 
+let reset = document.createElement("button");
+reset.innerHTML = "reset"
+reset.setAttribute("id", "button2")
+shooping.appendChild(reset)
+
+console.log(reset)
+
+
+
+
+/////now calling button reste then finction then event Listener
+
+
+function clearAll(){
+    let oldList = document.getElementsByTagName("li");    
+    oldList.shooping.removeChild(oldList);
+    // while (oldList.hasChildNodes()) {  
+    //     oldList.removeChild(oldList.firstChild);
+    //   }
+};
+
+
+let resetList= document.getElementById("button2");
+
+//tel the button what todo 
+resetList.addEventListener('click', clearAll);
